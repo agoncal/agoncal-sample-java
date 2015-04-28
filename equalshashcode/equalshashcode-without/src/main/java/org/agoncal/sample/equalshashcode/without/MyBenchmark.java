c27457690bfb0bd74d6d556e68449c8cd2a2fccc
Antonio Goncalves <antonio.goncalves@gmail.com>
2015-04-29 00:06:16 +0200

    Adding Benchmark

--- equalshashcode/equalshashcode-without/src/main/java/org/agoncal/sample/equalshashcode/without/MyBenchmark.java
+++ equalshashcode/equalshashcode-without/src/main/java/org/agoncal/sample/equalshashcode/without/MyBenchmark.java
@@ -17,14 +17,16 @@ public class MyBenchmark {
         Book book;
         CD cd;
 
-        // Adds a book
-        book = new Book(Lorem.getWords(1), Lorem.getTitle(1, 10), random.nextFloat(), Lorem.getParagraphs(10, 50), random.nextInt(), new LocalDate(2014, 12, 4), Language.ENGLISH, new Category("Scifi"), new Author(Lorem.getFirstName(), Lorem.getLastName(), new LocalDate(1971, 5, 29), Lorem.getParagraphs(10, 100), Language.FRENCH), new Publisher(Lorem.getWords(3)));
-        inventory.addItems(book, 10);
-        inventory.getStock().get(book);
+        for (int i = 0; i < 10000; i++) {
+            // Adds a book
+            book = new Book(Lorem.getWords(1), Lorem.getTitle(1, 10), random.nextFloat(), Lorem.getParagraphs(10, 50), random.nextInt(), new LocalDate(2014, 12, 4), Language.ENGLISH, new Category("Scifi"), new Author(Lorem.getFirstName(), Lorem.getLastName(), new LocalDate(1971, 5, 29), Lorem.getParagraphs(10, 100), Language.FRENCH), new Publisher(Lorem.getWords(3)));
+            inventory.addItems(book, 10);
+            inventory.getStock().get(book);
 
-        // Adds a CD
-        cd = new CD(Lorem.getWords(1), Lorem.getTitle(1, 10), random.nextFloat(), Lorem.getParagraphs(10, 50), random.nextFloat(), new MajorLabel("EMI"), new Genre("Rock"));
-        inventory.addItems(cd, 10);
-        inventory.getStock().get(cd);
+            // Adds a CD
+            cd = new CD(Lorem.getWords(1), Lorem.getTitle(1, 10), random.nextFloat(), Lorem.getParagraphs(10, 50), random.nextFloat(), new MajorLabel("EMI"), new Genre("Rock"));
+            inventory.addItems(cd, 10);
+            inventory.getStock().get(cd);
+        }
     }
 }
